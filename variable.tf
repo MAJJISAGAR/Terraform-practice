2bee27ebde8b418b0d615b97e891a6969032ad8e
MAJJISAGAR <sagarmajji143@gmail.com>
2025-05-29 16:16:31 +0530

    add these file

--- variable.tf
+++ variable.tf
@@ -7,3 +7,13 @@ variable "ec2-ami" {
   type = string
   default = "ami-0af9569868786b23a"
 }
+
+variable "vpc-cidr" {
+  type = string
+  default = "192.168.0.0/24"
+}
+
+variable "sub-cidr" {
+  type = string
+  default = "192.168.0.0/26"
+}
