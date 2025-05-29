1f2716c1175890bff7eab403dd4a7bfe9add0b61
MAJJISAGAR <sagarmajji143@gmail.com>
2025-05-29 16:55:27 +0530

    add az-1a

--- variable.tf
+++ variable.tf
@@ -1,12 +1,13 @@
 variable "ec2-type" {
   type = string
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "ec2-ami" {
   type = string
   default = "ami-0af9569868786b23a"
 }
+<<<<<<< HEAD
 
 variable "vpc-cidr" {
   type = string
@@ -17,3 +18,5 @@ variable "sub-cidr" {
   type = string
   default = "192.168.0.0/26"
 }
+=======
+>>>>>>> f48a4c7bcfcb02309af2172ec3f16c84ade62a84
